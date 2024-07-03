afbea284a1f1a1ee513604eac1e747c774322d89
wodydl0 <poty9798@naver.com>
2024-07-03 13:31:33 +0900

    init: first commit

---
+++ src/test/java/com/example/rightchain/RightchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rightchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
