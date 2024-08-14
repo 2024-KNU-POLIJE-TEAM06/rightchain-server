ad42aa6a83d676a045bc2b1a1e032b3a65dc9e88
wodydl0 <poty9798@naver.com>
2024-08-15 02:43:54 +0700

    feat: fix conficuration of CORS

--- src/main/java/com/example/rightchain/config/SecurityConfig.java
+++ src/main/java/com/example/rightchain/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.example.rightchain.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -30,6 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .sessionManagement(AbstractHttpConfigurer::disable)
